d168208b3ccfb8e8a35c6bc174f59b7afd56d2a2
Emer Rutherford <emer.rutherford@nearform.com>
2016-09-15 16:59:38 +0100

    Added seneca-log-filter.

--- logentries.js
+++ logentries.js
@@ -1,12 +1,15 @@
 'use strict'
 
 var LELogger = require('le_node')
+var LogFilter = require('seneca-log-filter')
 
 function Logger () {}
 
 Logger.preload = function () {
   var seneca = this
   var options = seneca.options()
+
+  var logFilter = LogFilter(options['log'])
   var logentries = options['logentries-logger']
   if (logentries && !logentries.levels) {
     // Match the Seneca log levels:
@@ -16,7 +19,10 @@ Logger.preload = function () {
   var logger = new LELogger(logentries)
 
   function adapter (context, payload) {
-    logger[payload.level](payload)
+    let filtered = logFilter(payload)
+    if(filtered) {
+      logger[payload.level](payload)
+    }
   }
 
   return {
