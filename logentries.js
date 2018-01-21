6219249a61870419e9369a6fa0cac092cb19d193
EamonnK <amen_cionna@hotmail.com>
2018-01-21 13:24:15 +0000

    Pre-commit hook removed.

--- logentries.js
+++ logentries.js
@@ -2,20 +2,20 @@
 
 var LELogger = require('le_node')
 
-function Logger () {}
+function Logger() {}
 
-Logger.preload = function () {
+Logger.preload = function() {
   var seneca = this
   var options = seneca.options()
   var logentries = options['logentries-logger']
   if (logentries && !logentries.levels) {
     // Match the Seneca log levels:
-    logentries.levels = {debug: 0, info: 1, warn: 2, error: 3, fatal: 4}
+    logentries.levels = { debug: 0, info: 1, warn: 2, error: 3, fatal: 4 }
   }
 
   var logger = new LELogger(logentries)
 
-  function adapter (context, payload) {
+  function adapter(context, payload) {
     logger[payload.level](payload)
   }
 
